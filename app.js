4daf42d7039e42f0596aad6067d38b136722eeae
Makks <ivankucer04@gmail.com>
2021-10-02 12:37:53 +0300

    Bank System

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 alert('Hello')
+const money = prompt('How much money do you have ?')
+const periode = prompt('What priode ypu want to keep your money in our bank ?')
+const presantage = prompt('Precentage of a year')
+
+console.log(money, periode, presantage)
+
+
+result = money*(1+presantage/100) **periode 
+profit = result - money
+
+alert(result.toFixed(2))
+alert(profit.toFixed(2))
