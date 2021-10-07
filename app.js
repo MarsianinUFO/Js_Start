fad5f2c073261dcf3f43912281541794f45bc5d1
Makks <ivankucer04@gmail.com>
2021-10-07 17:59:57 +0300

    IF and ELSE theme

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-alert('Hello')
-const money = prompt('How much money do you have ?')
-const periode = prompt('What priode ypu want to keep your money in our bank ?')
-const presantage = prompt('Precentage of a year')
+// alert('Hello')
+// const money = prompt('How much money do you have ?')
+// const periode = prompt('What priode ypu want to keep your money in our bank ?')
+// const presantage = prompt('Precentage of a year')
 
-console.log(money, periode, presantage)
+// console.log(money, periode, presantage)
 
 
-result = money*(1+presantage/100) **periode 
-profit = result - money
+// const result = money*(1+presantage/100) **periode 
+// const profit = result - money
 
-alert(result.toFixed(2))
-alert(profit.toFixed(2))
+// alert(result.toFixed(2), profit.toFixed(2))
+
+
+
+const value = prompt('Enter Number: ')
+
+if (value <1) {
+    console.log(1)
+} else if (value > 1){ 
+    console.log(-1)
+} else{
+    console.log(0) 
+}
